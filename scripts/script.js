2fc6cf0c195d6f29d14ea20762de97c54120cc9d
pablobidiaki <pablobidiaki@hotmail.com>
2022-11-17 16:08:08 -0300

    altera

--- scripts/script.js
+++ scripts/script.js
@@ -6,16 +6,20 @@ let offset= 0
 function carregarPokemons(offset, limit){
     pokeApi.getPokemons(offset, limit).then((pokemons = []) =>{
         const newHtml = pokemons.map((pokemon) =>`
-            <li class="pokemon ${pokemon.type}">
-                <span class="number">#${pokemon.number}</span>
-                <span class="name">${pokemon.nome}</span>
-                <div class="detalhes">
-                    <ul class="tipos">
-                        ${pokemon.types.map((type) => `<li class="elemento">${type}</li>`).join('')}
-                    </ul>
-                    <img src="${pokemon.photo}" alt="">
-                </div>
-            </li>
+    <div class="modalContainer">
+            <div id="modal">
+                <li class="pokemon ${pokemon.type}" onclick="openModal()">
+                    <span class="number">#${pokemon.number}</span>
+                    <span class="name">${pokemon.nome}</span>
+                    <div class="detalhes">
+                        <ul class="tipos">
+                            ${pokemon.types.map((type) => `<li class="elemento">${type}</li>`).join('')}
+                        </ul>
+                        <img src="${pokemon.photo}" alt="">
+                    </div>
+                </li>
+            </div>
+        </div>
         `).join('')
         pokemonList.innerHTML +=newHtml
     })
@@ -27,3 +31,10 @@ carregarMais.addEventListener('click',() =>{
     offset += limit  
     carregarPokemons(offset, limit)
 })
+
+function openModal(){
+    const newHtml = pokemons.map((pokemon) =>`
+    alert
+        `).join('')
+        pokemonList.innerHTML +=newHtml
+}
